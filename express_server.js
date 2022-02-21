eeea33d48c59c80233453b9d4b5f1247d149e9f3
BBrandford11 <brandfordbryce@gmail.com>
2022-02-21 16:28:09 -0700

    added short url route

--- express_server.js
+++ express_server.js
@@ -11,6 +11,12 @@ app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase}
   res.render("urls_index", templateVars)
 })
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL
+  let longURL = urlDatabase[shortURL]
+  const templateVars = { shortURL, longURL };
+  res.render("urls_show", templateVars);
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
